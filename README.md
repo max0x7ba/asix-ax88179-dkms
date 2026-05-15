c60fde23b2b0f3de554c53778683d837250b3729
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2026-05-15 03:22:46 +0100

    Allow building from an existing tarball.

--- README.md
+++ README.md
@@ -1,21 +1,50 @@
 # ASIX AX88179 DKMS driver
 
-This program will install the official ASIX AX88179 driver from the [manufacturer's website](https://www.asix.com.tw/en/product/USBEthernet/Super-Speed_USB_Ethernet/AX88179A) using [DKMS](https://github.com/dell/dkms).
+This program installs the original ASIX AX88179 driver from [manufacturer's website][1] using [DKMS](https://github.com/dell/dkms).
+
+Installing the original driver may be required when a compatible driver loaded by a Linux distro lacks support for AX88179 features, such as full-duplex mode for 1Gb/s connections.
 
 ## How to install
 
-> Obviously, you need an Internet connection to allow the installation program to download the driver's source code tarball.
+1. Clone this repository and make it the working directory:
+```
+git clone https://github.com/FlorianLaunay/asix-ax88179-dkms.git
+cd asix-ax88179-dkms
+```
 
-1. Clone this repository: `git clone https://github.com/FlorianLaunay/asix-ax88179-dkms.git && cd asix-ax88179-dkms`
-2. Checkout to wanted version: `git checkout 3.4.0`
-3. Run the installation program: `sudo ./install.sh`
-4. Connect/reconnect your device
+2. (Optional step) Navigate to [AX88179 product page][1] and download the Linux driver. Copy the downloaded tarball path into the clipboard for the next step.
+
+3. Run the installation program to download, build and install the driver.
+```
+sudo ./install.sh
+```
+
+To install from an existing tarball (downloaded in optional step 2), specify the tarball path as its last argument. E.g.:
+```
+sudo ./install.sh ~/Downloads/ASIX_USB_NIC_Linux_Driver_Source_v4.1.0.tar.bz2
+```
+
+4. Unplug your USB device with ASIX AX88179. Wait a few seconds. Plug it back in.
+
+5. Verify that your USB device with ASIX AX88179 is now managed by its original driver:
+```
+lsusb -vt | grep -B1 AX88179
+```
+Which should report `Driver=ax_usb_nic`:
+```
+        |__ Port 004: Dev 004, If 0, Class=Vendor Specific Class, Driver=ax_usb_nic, 5000M
+            ID 0b95:1790 ASIX Electronics Corp. AX88179 Gigabit Ethernet
+```
 
 ## How to uninstall
 
-3. Simply run the installation program with the uninstall option: `sudo ./install.sh -u`
-4. Connect/reconnect your device
+1. Run the installation program with the uninstall option. If necessary, specify the existing tarball path as its last argument:
+```
+sudo ./install.sh -u [<path-to-source-tarball>]
+```
 
 ## Licence
 
 Please see [LICENCE](LICENCE).
+
+[1]: https://www.asix.com.tw/en/product/USBEthernet/Super-Speed_USB_Ethernet/AX88179A
--- install.sh
+++ install.sh
@@ -18,86 +18,118 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-pkgname="asix-ax88179"
-pkgver="$(git describe --tags 2> /dev/null || echo 'No tag found' >&2)"
-archivename="ASIX_USB_NIC_Linux_Driver_Source_v${pkgver}"
-archiveurl="https://www.asix.com.tw/en/support/download/file/2150"
-drvname="ax_usb_nic"
-srcdir="/usr/src/${pkgname}-${pkgver}"
-log="/var/lib/dkms/${pkgname}/${pkgver}/build/make.log"
+set -euo pipefail # Terminate on errors.
+
+readonly pkgname="asix-ax88179"
+readonly archiveurl="https://www.asix.com.tw/en/support/download/file/2150"
+readonly drvname="ax_usb_nic"
+
+error() {
+  local cR="\e[91m" cD="\e[0m"
+  printf "${cR}Error:${cD} %s\n" "$1" >&2
+  exit 1
+}
+
+# An existing tarball can be specified as the last argument. E.g.:
+#   cd ~/src/asix-ax88179-dkms/ && sudo ./install.sh ~/Downloads/ASIX_USB_NIC_Linux_Driver_Source_v4.1.0.tar.bz2
+#   cd ~/src/asix-ax88179-dkms/ && sudo ./install.sh -u ~/Downloads/ASIX_USB_NIC_Linux_Driver_Source_v4.1.0.tar.bz2
+#   cd ~/src/asix-ax88179-dkms-max0x7ba/ && sudo ./install.sh ~/Downloads/ASIX_USB_NIC_Linux_Driver_Source_v4.1.0.tar.bz2
+readonly argv=("$0" "$@")
+readonly last_arg="${argv[-1]}"
+[[ "$last_arg" != *.tar.bz2 ]] || readonly tarball="$last_arg"
+
+if [[ -v tarball ]]; then
+  [[ -s "$tarball" && "$tarball" =~ ([^/]+_v([^.]+\.[^.]+\.[^.]+)\.tar\.bz2)$ ]] || error "Invalid value: $tarball"
+  readonly pkgver="${BASH_REMATCH[2]}"
+  readonly archivename="${BASH_REMATCH[1]}"
+else
+  readonly pkgver="$(git describe --tags 2> /dev/null || error 'No tag found')"
+  readonly archivename="ASIX_USB_NIC_Linux_Driver_Source_v${pkgver}"
+fi
+[[ "$pkgver" ]] || error "Invalid value: pkgver=$pkgver"
+
+readonly srcdir="/usr/src/${pkgname}-${pkgver}"
+readonly log="/var/lib/dkms/${pkgname}/${pkgver}/build/make.log"
 
 usage() {
-  echo "Usage: $0 [options]"
-  echo
-  echo 'Options:'
-  echo '  -h  Display usage and exit'
-  echo '  -u  Uninstall'
-  echo
-  echo 'Example:'
-  echo "  to install: $0"
-  echo "  to uninstall: $0 -u"
+  cat <<EOF
+Usage: $0 [options] [<path-to-source-tarball>]
+
+Options:
+  -h  Display usage and exit
+  -u  Uninstall
+
+Example:
+  to install: $0
+  to install from a tarball: $0 <path-to-source-tarball>
+  to uninstall: $0 -u
+
+EOF
 }
 
 build() {
-  echo "Downloading sources and configuring DKMS..."
-  mkdir -p "${srcdir}"
-  if wget -q --show-progress -O - "${archiveurl}" | tar -xj -C "${srcdir}" --no-same-owner --transform "s/${archivename}/./g"; then
-    install -D -m 644 dkms.conf "${srcdir}/dkms.conf"
-    sed -i "s/#PKGNAME#/${pkgname}/" "${srcdir}/dkms.conf"
-    sed -i "s/#PKGVER#/${pkgver}/" "${srcdir}/dkms.conf"
-    sed -i "s/#DRVNAME#/${drvname}/" "${srcdir}/dkms.conf"
+  untar || error "Download error"
+
+  echo "Configuring DKMS..."
+  install -D -m 644 dkms.conf "${srcdir}/dkms.conf"
+  sed -i "s/#PKGNAME#/${pkgname}/" "${srcdir}/dkms.conf"
+  sed -i "s/#PKGVER#/${pkgver}/" "${srcdir}/dkms.conf"
+  sed -i "s/#DRVNAME#/${drvname}/" "${srcdir}/dkms.conf"
+  echo -e "Done\n"
+
+  echo "Building with DKMS..."
+  if dkms install -m "${pkgname}" -v "${pkgver}"; then
     echo -e "Done\n"
-    
-    echo "Building with DKMS..."
-    if dkms install -m "${pkgname}" -v "${pkgver}"; then
-      echo -e "Done\n"
-    
-      echo "Configuring driver..."
-      install -D -m 644 udev.rules "/etc/udev/rules.d/90-${pkgname}.rules"
-      install -D -m 644 modprobe-blacklist.conf "/etc/modprobe.d/${pkgname}.conf"
-      modprobe -r ax88179_178a
-      udevadm control --reload-rules && udevadm trigger
-      echo "Done"
-    else
-      echo "Build error" >&2
-      if [ -r "$log" ]; then
-        cat "$log" >&2
-      fi
-    
-      exit 1
-    fi
+
+    echo "Configuring driver..."
+    install -D -m 644 udev.rules "/etc/udev/rules.d/90-${pkgname}.rules"
+    install -D -m 644 modprobe-blacklist.conf "/etc/modprobe.d/${pkgname}.conf"
+    modprobe -r ax88179_178a
+    udevadm control --reload-rules && udevadm trigger
+    echo "Done"
   else
-    echo "Download error" >&2
-    exit 1
+    cat "$log" || :
+    error "Build error"
   fi
 }
 
 uninstall() {
-  status="$(dkms status ${pkgname})"
-  if [ -n "$status" ]; then
-    echo "Uninstalling driver ${pkgname} version ${pkgver}..."
-    modprobe -r "${drvname}"
-    dkms remove -m "${pkgname}" -v "${pkgver}"
-    rm -rf "${srcdir}"
-    rm -f "/etc/udev/rules.d/90-${pkgname}.rules"
-    rm -f "/etc/modprobe.d/${pkgname}.conf"
-    modprobe ax88179_178a
-    udevadm control --reload-rules && udevadm trigger
-    echo "Done"
+  set +e # Ignore errors here.
+
+  local status="$(dkms status ${pkgname})"
+  [[ "$status" ]] || error "Driver ${pkgname} is not installed!"
+
+  echo "Uninstalling driver ${pkgname} version ${pkgver}..."
+  modprobe -r "${drvname}"
+  dkms remove -m "${pkgname}" -v "${pkgver}"
+  rm -rf "${srcdir}"
+  rm -f "/etc/udev/rules.d/90-${pkgname}.rules"
+  rm -f "/etc/modprobe.d/${pkgname}.conf"
+  modprobe ax88179_178a
+  udevadm control --reload-rules && udevadm trigger
+  echo "Done"
+}
+
+untar() {
+  mkdir -p "${srcdir}"
+  local -r tar_cmd=(tar -x -C "${srcdir}" --no-same-owner --transform "s/${archivename}/./g")
+  if [[ -v tarball ]]; then
+    echo "Unpacking source tarball $tarball ..."
+    "${tar_cmd[@]}" -f "$tarball"
   else
-    echo "Driver ${pkgname} is not installed!" >&2
-    exit 1
+    echo "Downloading and unpacking source tarball from $archiveurl ..."
+    wget -q --show-progress -O - "${archiveurl}" | "${tar_cmd[@]}" -j
   fi
 }
 
 while getopts 'hu' opt; do
   case "${opt}" in
     h) usage; exit 0;;
-    u) [ -n "$pkgver" ] || exit 1; uninstall; exit 0 ;;
+    u) uninstall; exit 0 ;;
     *) usage >&2; exit 1 ;;
   esac
 done
 
-[ -n "$pkgver" ] || exit 1
 build
+
 # vim: set sw=2 sts=2 et :
